2e2e55be696e23bc5a689248fa54eabb8e6c0198
Emmanuel Roussel <emman.roussel@gmail.com>
2016-12-17 16:05:03 -0500

    Remove unused routes

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -14,20 +14,6 @@ router.prefix(`/${baseApi}/${api}`)
 
 /* eslint-disable no-unused-vars, no-param-reassign, new-cap */
 
-// GET /api/movies
-router.get('/', async(ctx) =>
-  ctx.body = await City.find())
-
-// POST /api/movies
-router.post('/', async(ctx) => {
-  try {
-    const city = await new City(ctx.request.body).save()
-    ctx.body = city
-  } catch (err) {
-    ctx.throw(422)
-  }
-})
-
 // GET /api/movies/search
 router.get('/:search/:page', async(ctx) => {
   try {
@@ -38,38 +24,6 @@ router.get('/:search/:page', async(ctx) => {
     const movies = await Request.get(movieUrl)
 
     ctx.body = JSON.parse(movies)
-  } catch (err) {
-    if (err.name === 'CastError' || err.name === 'NotFoundError') {
-      ctx.throw(404)
-    }
-    ctx.throw(500)
-  }
-})
-
-// PUT /api/movies/id
-router.put('/:id', async(ctx) => {
-  try {
-    const city = await City.findByIdAndUpdate(ctx.params.id, ctx.request.body)
-    if (!city) {
-      ctx.throw(404)
-    }
-    ctx.body = city
-  } catch (err) {
-    if (err.name === 'CastError' || err.name === 'NotFoundError') {
-      ctx.throw(404)
-    }
-    ctx.throw(500)
-  }
-})
-
-// DELETE /api/movies/id
-router.delete('/:id', async(ctx) => {
-  try {
-    const city = await City.findByIdAndRemove(ctx.params.id)
-    if (!city) {
-      ctx.throw(404)
-    }
-    ctx.body = city
   } catch (err) {
     if (err.name === 'CastError' || err.name === 'NotFoundError') {
       ctx.throw(404)
--- test/api.test.js
+++ test/api.test.js
@@ -6,35 +6,6 @@ const temp = {}
 const request = supertest.agent(app.listen())
 should()
 
-describe('POST /movies', () => {
-  it('should add a city', (done) => {
-    request
-      .post('/api/movies')
-      .set('Accept', 'application/json')
-      .send({
-        name: 'Bangkok',
-        totalPopulation: 8249117,
-        country: 'Thailand',
-        zipCode: 1200,
-      })
-      .expect(200, (err, res) => {
-        temp.idCity = res.body._id;
-        done()
-      })
-  })
-})
-
-describe('GET /movies', () => {
-  it('should get all movies', (done) => {
-    request
-      .get('/api/movies')
-      .expect(200, (err, res) => {
-        expect(res.body.length).to.be.at.least(1);
-        done()
-      })
-  })
-})
-
 describe('GET /movies/:id', () => {
   it('should get a movie', (done) => {
     request
@@ -45,56 +16,6 @@ describe('GET /movies/:id', () => {
         res.body.country.should.equal('Thailand')
         res.body.zipCode.should.equal(1200)
         res.body._id.should.equal(temp.idCity)
-        done()
-      })
-  })
-})
-
-describe('PUT /movies', () => {
-  it('should update a movie', (done) => {
-    request
-      .put(`/api/movies/${temp.idCity}`)
-      .set('Accept', 'application/json')
-      .send({
-        name: 'Chiang Mai',
-        totalPopulation: 148477,
-        country: 'Thailand',
-        zipCode: 50000,
-      })
-      .expect(200, (err, res) => {
-        temp.idCity = res.body._id;
-        done()
-      })
-  })
-
-  it('should get updated movie', (done) => {
-    request
-      .get(`/api/movies/${temp.idCity}`)
-      .expect(200, (err, res) => {
-        res.body.name.should.equal('Chiang Mai')
-        res.body.totalPopulation.should.equal(148477)
-        res.body.country.should.equal('Thailand')
-        res.body.zipCode.should.equal(50000)
-        res.body._id.should.equal(temp.idCity)
-        done()
-      })
-  })
-})
-
-describe('DELETE /movies', () => {
-  it('should delete a movie', (done) => {
-    request
-      .delete(`/api/movies/${temp.idCity}`)
-      .set('Accept', 'application/json')
-      .expect(200, (err, res) => {
-        done()
-      })
-  })
-
-  it('should get error', (done) => {
-    request
-      .get(`/api/movies/${temp.idCity}`)
-      .expect(404, () => {
         done()
       })
   })
